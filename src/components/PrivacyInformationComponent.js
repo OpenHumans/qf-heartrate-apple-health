38b3f4c8c588f378fc02e67577d98fecd3d021a9
lbaldy <>
2020-03-29 21:03:08 +0200

    add privacy policy page layout, open an external website to read more.

--- src/components/PrivacyInformationComponent.js
+++ src/components/PrivacyInformationComponent.js
@@ -1,10 +1,40 @@
 import React from 'react';
-import {View, Text, SafeAreaView} from 'react-native';
+import {
+  TouchableOpacity,
+  Text,
+  SafeAreaView,
+  Linking,
+  View,
+} from 'react-native';
+import {I18N, PRIVACY_POLICY_URL} from '../common/config';
 
 const PrivacyInformationComponent = () => {
   return (
     <SafeAreaView style={{flex: 1, backgroundColor: '#5D71BA'}}>
-      <Text style={{color: '#FFF', padding: 10}}>Lorem Ipsum dolor sit amet</Text>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'center',
+          alignItems: 'center',
+          padding: 15,
+        }}>
+        <Text style={{fontSize: 16, color: '#FFF'}}>
+          {I18N.PRIVACY_POLICY_TEXT}
+          <Text
+            style={{
+              fontWeight: 'bold',
+              textDecorationLine: 'underline',
+            }}
+            onPress={() => {
+              Linking.openURL(PRIVACY_POLICY_URL).catch(err =>
+                console.error('An error occurred', err),
+              );
+            }}>
+            here
+          </Text>
+            .
+        </Text>
+      </View>
     </SafeAreaView>
   );
 };
